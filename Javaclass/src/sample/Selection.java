342dc68cc1b2642cdf1e4379ce1df307b97f5ddf
LEEJUNGJIN <twostop21@knu.ac.kr>
2016-03-21 21:07:27 +0900

    gggggggggggggggg

---
+++ Javaclass/src/sample/Selection.java
@@ -0,0 +1,14 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Selection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input;
+		Scanner input = new Scanner(System.in);
+		
+	}
+
+}
